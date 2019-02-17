2852bce92a4c96b88c24e799c89256462d0f62c8
Markos András <markos.andras@ak-akademia.hu>
2019-02-17 12:28:22 +0100

    updated sqls

---
+++ BuddyFinder/res/db/4-create-orientation.sql
@@ -0,0 +1,11 @@
+--DROP TABLE orientation;
+
+CREATE TABLE orientation (
+    orientation_id     NUMBER(10) NOT NULL,
+    orientation_name   VARCHAR2(20) NOT NULL,
+    CONSTRAINT orientation_pk PRIMARY KEY (orientation_id)
+);
+
+INSERT INTO orientation (orientation_id,orientation_name) VALUES (1, 'HETEROSEXUAL');
+INSERT INTO orientation (orientation_id,orientation_name) VALUES (2, 'HOMOSEXUAL');
+INSERT INTO orientation (orientation_id,orientation_name) VALUES (3, 'BISEXUAL');
--- BuddyFinder/res/db/createOrientation.sql
+++
@@ -1,7 +0,0 @@
---DROP TABLE orientation;
-
-CREATE TABLE orientation (
-    orientation_id     NUMBER(10) NOT NULL,
-    orientation_name   VARCHAR2(20) NOT NULL,
-    CONSTRAINT orientation_pk PRIMARY KEY (orientation_id)
-);
